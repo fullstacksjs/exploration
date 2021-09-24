1dc5b4c2d590d9e8b7ae20b3eb5c3b8b9aeb3e31
Alireza Safaierad <frontendmonster@gmail.com>
2021-09-25 03:59:16 +0400

    feat: initiate tooling and home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,25 @@
+/* eslint-disable camelcase */
+const site = require('./config/site.json');
+
+/** @type import('gatsby').GatsbyConfig */
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "exploration",
+    siteUrl: site.url,
+    title: site.title,
   },
   plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-theme-ui',
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        accessToken: "d24hV-WZ21qNRIMqiljBSKeE8q4n6eqFZbP6mL0-oKo",
-        spaceId: "",
-      },
-    },
-    "gatsby-plugin-theme-ui",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "G-FPV6XZM1SE",
-      },
-    },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
+        name: site.title,
+        short_name: site.shortName,
+        start_url: `/`,
+        background_color: site.backgroundColor,
+        theme_color: site.themeColor,
+        display: `standalone`,
       },
     },
   ],
---
+++ src/gatsby-plugin-theme-ui/theme.ts
@@ -0,0 +1,46 @@
+import { range } from '@fullstacksjs/toolbox';
+import { Theme } from 'theme-ui';
+
+const theme: Theme = {
+  colors: {
+    primary: {
+      __default: 'hsla(150, 80%, 61%, 1)',
+    },
+    text: {
+      __default: '#FFFFFF',
+    },
+    background: {
+      0: '#242932',
+      1: '#313744',
+      2: '#383E4D',
+      3: '#404859',
+    },
+  },
+  space: range(21).map((i) => i * 4),
+  fontSizes: [12, '0.875rem', '1.25rem', '2.625rem', 56],
+  fonts: {
+    monospace: 'Menlo, monospace',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 500,
+  },
+  text: {
+    default: {
+      fontSize: 1,
+      fontWeight: 'normal',
+    },
+    h1: {
+      fontSize: 3,
+      fontWeight: 'bold',
+    },
+  },
+  breakpoints: ['576px', '768px', '992px', '1200px', '1440px'],
+  layout: {
+    container: {
+      maxWidth: ['100%', '540px', '720px', '960px', '1170px', '1440px'],
+    },
+  },
+};
+
+export default theme;
