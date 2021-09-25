9429c00a7bffa0ee76b8a4fb68ec05e0e99cfe9d
Alireza Safaierad <frontendmonster@gmail.com>
2021-09-25 13:53:13 +0400

    fix: fix wrong font weight

--- src/gatsby-plugin-theme-ui/theme.ts
+++ src/gatsby-plugin-theme-ui/theme.ts
@@ -23,8 +23,12 @@ const theme: Theme = {
     monospace: 'Menlo, monospace',
   },
   fontWeights: {
-    normal: 400,
-    bold: 500,
+    normal: 500,
+    bold: 600,
+    heading: 600,
+  },
+  lineHeights: {
+    heading: '1.3',
   },
   text: {
     default: {
@@ -33,7 +37,6 @@ const theme: Theme = {
     },
     h1: {
       fontSize: 3,
-      fontWeight: 'bold',
     },
   },
   breakpoints: ['576px', '768px', '992px', '1200px', '1440px'],
