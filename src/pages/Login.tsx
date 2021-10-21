7ef79ae8d606cf7251d754f7ebac95fb4cd40e3a
BeardyDeveloper <beardydeveloper@gmail.com>
2021-10-21 19:34:21 +0330

    develop login page mobile

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,12 +20,28 @@ const Login = () => {
           gap: 20,
         }}
       >
-        <Card>
+        <Card
+          sx={{
+            width: ['95%', 'auto'],
+            height: ['calc(100vh - 35px)', 'auto'],
+            position: 'relative',
+            display: ['flex', 'block'],
+            flexDirection: ['column', 'unset'],
+            justifyContent: ['center', 'unset'],
+            background: [
+              'none',
+              `linear-gradient(to right, transparent 70%, hsla(219, 40%, 88%, 0.2) 0%) top/13px 1px repeat-x,
+            linear-gradient(transparent 70%, hsla(219, 40%, 88%, 0.2) 0%) right/1px 13px repeat-y,
+            linear-gradient(to right, transparent 70%, hsla(219, 40%, 88%, 0.2) 0%) bottom/13px 1px repeat-x,
+            linear-gradient(transparent 70%, hsla(219, 40%, 88%, 0.2) 0%) left/1px 13px repeat-y`,
+            ],
+          }}
+        >
           <Flex sx={{ flexDirection: 'column', gap: 4 }}>
             <Flex
               sx={{
                 flexDirection: 'column',
-                width: '333px',
+                width: ['100%', '333px'],
                 gap: 8,
                 alignItems: 'center',
               }}
@@ -36,7 +52,7 @@ const Login = () => {
                 sx={{ flexDirection: 'column', gap: 2, alignItems: 'center' }}
               >
                 <Heading as="h2">Welcome</Heading>
-                <Text sx={{ textAlign: 'justify' }} color="text.1">
+                <Text sx={{ textAlign: ['center', 'justify'] }} color="text.1">
                   The Fullstacks Exploration is about to begin. Join us on
                   Discord if you want to learn something new.
                 </Text>
@@ -45,6 +61,15 @@ const Login = () => {
             <GithubButton
               onClick={() => {
                 navigate('/Topics');
+              }}
+              sx={{
+                position: ['absolute', 'unset'],
+                bottom: 0,
+                left: 0,
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
             >
               Login with Github
