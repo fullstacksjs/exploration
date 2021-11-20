c92844c82d482ff918ca85db5616bd009f5abb91
Alireza Safaierad <frontendmonster@gmail.com>
2021-11-20 22:10:30 +0400

    fix: fix fetcher error

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,2 +1,8 @@
 export const fetcher = (...args: Parameters<typeof fetch>) =>
-  fetch(...args).then((res) => res.json());
+  fetch(...args).then((res) => {
+    return res.ok
+      ? res.json()
+      : res.json().then((json) => {
+          throw json;
+        });
+  });
